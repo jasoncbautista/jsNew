948d399913b90f05f8b7235e50349d829565214e
Jason <bjason@gmail.com>
2014-12-29 02:21:45 -0800

    templates

--- jsNew.py
+++ jsNew.py
@@ -4,15 +4,18 @@ import sys
 
 def makeDirectory(directory_name):
     if not os.path.exists(directory_name):
+        print "Made directory.."
         os.makedirs(directory_name)
     else:
-        print "Directory already exists"
+        print "ERROR: Directory already exists"
+        sys.exit(0)
 
 
 def run(directory_name):
     dir_path = "%s/%s/" % ( os.getcwd(),  directory_name)
     print "DIR to make %s"  % dir_path
     makeDirectory(dir_path)
+    # Clone our sample app, which is this scrip itself =-)
 
 
 def main():
