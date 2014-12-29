13871ccde11028c7d27764532cb58aced7848cb6
Jason <bjason@gmail.com>
2014-12-29 02:04:51 -0800

    main

--- jsNew.py
+++ jsNew.py
@@ -2,5 +2,17 @@ import os
 
 
 def makeDirectory(directory_name):
-    if not os.path.exists(directory_name)
+    if not os.path.exists(directory_name):
         os.makedirs(directory_name)
+
+
+
+def main():
+    # Run shit here
+    print "yo"
+
+
+if __name__ == "__main__":
+    main()
+
+
